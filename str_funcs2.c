92c9e7d151a407bfc579f5d6c231f82f532afc13
larsody <larsody@gmail.com>
2022-08-07 09:01:20 -0700

    str_functions

--- str_funcs2.c
+++ str_funcs2.c
@@ -50,3 +50,49 @@ s++;
 }
 return (bytes);
 }
+
+/**
+* _strcmp - Compares two strings.
+* @s1: The first string to be compared.
+* @s2: The second string to be compared.
+*
+* Return: Positive byte difference if s1 > s2
+*         0 if s1 = s2
+*         Negative byte difference if s1 < s2
+*/
+int _strcmp(char *s1, char *s2)
+{
+while (*s1 && *s2 && *s1 == *s2)
+{
+s1++;
+s2++;
+}
+if (*s1 != *s2)
+return (*s1 - *s2);
+return (0);
+}
+/**
+* _strncmp - Compare two strings.
+* @s1: Pointer to a string.
+* @s2: Pointer to a string.
+* @n: The first n bytes of the strings to compare.
+*
+* Return: Less than 0 if s1 is shorter than s2.
+*         0 if s1 and s2 match.
+*         Greater than 0 if s1 is longer than s2.
+*/
+int _strncmp(const char *s1, const char *s2, size_t n)
+{
+size_t i;
+for (i = 0; s1[i] && s2[i] && i < n; i++)
+{
+if (s1[i] > s2[i])
+return (s1[i] - s2[i]);
+else if (s1[i] < s2[i])
+return (s1[i] - s2[i]);
+}
+if (i == n)
+return (0);
+else
+return (-15);
+}
