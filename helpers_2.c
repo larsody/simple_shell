ccec574b7751d1e153435b5195d85927fac5de00
larsody <larsody@gmail.com>
2022-08-07 09:23:32 -0700

    helpers

--- helpers_2.c
+++ helpers_2.c
@@ -93,3 +93,93 @@ return;
 	free(*line);
 	*line = new_line;
 }
+
+/**
+* get_new_len - Gets the new length of a line partitioned
+*               by ";", "||", "&&&", or "#".
+* @line: The line to check.
+*
+* Return: The new length of the line.
+*
+* Description: Cuts short lines containing '#' comments with '\0'.
+*/
+
+ssize_t get_new_len(char *line)
+{
+	size_t i;
+	ssize_t new_len = 0;
+	char current, next;
+
+	for (i = 0; line[i]; i++)
+	{
+		current = line[i];
+		next = line[i + 1];
+		if (current == '#')
+		{
+			if (i == 0 || line[i - 1] == ' ')
+			{
+				line[i] = '\0';
+				break;
+			}
+		}
+		else if (i != 0)
+		{
+			if (current == ';')
+			{
+				if (next == ';' && line[i - 1] != ' ' && line[i - 1] != ';')
+				{
+					new_len += 2;
+					continue;
+				}
+				else if (line[i - 1] == ';' && next != ' ')
+				{
+					new_len += 2;
+					continue;
+				}
+				if (line[i - 1] != ' ')
+					new_len++;
+				if (next != ' ')
+					new_len++;
+			}
+			else
+				logical_ops(&line[i], &new_len);
+		}
+		else if (current == ';')
+		{
+			if (i != 0 && line[i - 1] != ' ')
+				new_len++;
+			if (next != ' ' && next != ';')
+				new_len++;
+		}
+		new_len++;
+	}
+	return (new_len);
+}
+/**
+* logical_ops - Checks a line for logical operators "||" or "&&".
+* @line: A pointer to the character to check in the line.
+* @new_len: Pointer to new_len in get_new_len function.
+*/
+void logical_ops(char *line, ssize_t *new_len)
+{
+	char previous, current, next;
+
+	previous = *(line - 1);
+	current = *line;
+	next = *(line + 1);
+
+	if (current == '&')
+	{
+		if (next == '&' && previous != ' ')
+			(*new_len)++;
+		else if (previous == '&' && next != ' ')
+			(*new_len)++;
+	}
+	else if (current == '|')
+	{
+		if (next == '|' && previous != ' ')
+			(*new_len)++;
+		else if (previous == '|' && next != ' ')
+			(*new_len)++;
+	}
+}
