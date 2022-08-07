a61acc273713e6a5d59d0547b553fbc84ffc194c
Oginn <oginniadeniyi@gmail.com>
2022-08-07 09:22:46 -0700

    errors

--- errors.c
+++ errors.c
@@ -63,3 +63,41 @@ len--;
 return (buffer);
 }
 
+/**
+* create_error - Writes a custom error message to stderr.
+* @args: An array of arguments.
+* @err: The error value.
+*
+* Return: The error value.
+*/
+int create_error(char **args, int err)
+{
+char *error;
+switch (err)
+{
+case -1:
+error = error_env(args);
+break;
+case 1:
+error = error_1(args);
+break;
+case 2:
+if (*(args[0]) == 'e')
+error = error_2_exit(++args);
+else if (args[0][0] == ';' || args[0][0] == '&' || args[0][0] == '|')
+error = error_2_syntax(args);
+else
+error = error_2_cd(args);
+break;
+case 126:
+error = error_126(args);
+break;
+case 127:
+error = error_127(args);
+break;
+}
+write(STDERR_FILENO, error, _strlen(error));
+if (error)
+free(error);
+return (err);
+}
