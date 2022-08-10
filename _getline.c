1bb11366e36e514469e57073e69931cafb18724c
Oginn <oginniadeniyi@gmail.com>
2022-08-10 14:31:57 -0700

    getline

--- _getline.c
+++ _getline.c
@@ -99,3 +99,117 @@ int _getline(char **lineptr, int fd)
 		/*printf("j: %d, i:%d, r:%d\n", j, i ,r);*/
 	}
 }
+	}
+		i++;
+	}
+	return (name);
+}
+/**
+ * _setenv - set environment for new value
+ * @name: name of variable
+ * @val: value of variable
+ * Return: 0 or setallenv if success, -1 if fail
+ */
+int _setenv(char *name, char *val)
+{
+	char ***environroot = getenviron();
+	char **environ = *environroot;
+	int i, j, namel, vall;
+	char *s, *ptr;
+
+#ifdef DEBUGMODE
+	printf("In setenv, name:%s:val:%s\n", name, val);
+#endif
+	if (name == NULL || val == NULL)
+		return (0);
+	namel = _strlen(name);
+	vall = _strlen(val);
+	ptr = malloc(sizeof(char) * (namel + vall + 2));
+	if (ptr == NULL)
+		return (-1);
+	s = ptr;
+	_strcpy(s, name);
+	s += namel;
+	_strcpy(s++, "=");
+	_strcpy(s, val);
+	s += vall;
+	*s = 0;
+#ifdef DEBUGMODE
+	printf("Ptr mallocd:%s\n", ptr);
+#endif
+	i = 0;
+	while (environ[i] != NULL)
+	{
+		s = environ[i];
+		j = 0;
+		while (s[j] == name[j])
+		{
+			j++;
+			if (name[j] == 0 && s[j] == '=')
+			{
+				free(environ[i]);
+				environ[i] = ptr;
+				return (0);
+			}
+		}
+		i++;
+	}
+	return (setallenv(*environroot, ptr));
+}
+/**
+ * _unsetenv - unset environment
+ * @name: name of variable to unset
+ * Return: 0 if sucess
+ *
+ * testing functionality  copy environ, if hits skip over, realloc
+ */
+int _unsetenv(char *name)
+{
+	char **environ = *getenviron();
+	int i, j;
+	int check = 0;
+	char *s;
+	char **env;
+
+#ifdef DEBUGMODE
+	printf("In unsetenv, name:%s\n", name);
+#endif
+	if (name == NULL)
+		return (0);
+
+	i = 0;
+	while (environ[i] != NULL)
+	{
+		s = environ[i];
+		j = 0;
+		while (s[j] == name[j])
+		{
+			j++;
+			if (s[j] == '=' && name[j] == '\0')
+			{
+				check = 1;
+				break;
+			}
+		}
+		if (check == 1)
+			break;
+		i++;
+	}
+	free(environ[i]);
+	while (environ[i] != NULL)
+	{
+		environ[i] = environ[i + 1];
+		i++;
+	}
+	environ[i] = NULL;
+	env = environ;
+	setallenv(env, NULL);
+	i = 0;
+	while (env[i])
+	{
+		free(env[i]);
+		i++;
+	}
+	free(env);
+	return (0);
+}
