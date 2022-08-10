563ca2832b1d7f814acbf0cabe93bce5923da96e
larsody <larsody@gmail.com>
2022-08-10 13:00:25 -0700

    update shell variables

--- shellvars.c
+++ shellvars.c
@@ -56,3 +56,162 @@ int initsvars(int ac, char **av)
 	ptr->next = NULL;
 	return (0);
 }
+/**
+ * getsvar - gets shell variable
+ * @name: name of shell var
+ *
+ * Return: original argument if not found
+ */
+char *getsvar(char *name)
+{
+	ShellVar *special = *(getspecial()), *vars = *(getvars());
+	ShellVar *ptr = special;
+
+	while (ptr != NULL && _strcmp(ptr->name, name))
+	{
+#ifdef DEBUGMODE
+		printf("Checked .%s. against .%s.\n", name, ptr->name);
+#endif
+		ptr = ptr->next;
+	}
+	if (ptr != NULL)
+	{
+#ifdef DEBUGMODE
+		printf("Returning special var %s:%s\n", ptr->name, ptr->val);
+#endif
+		return (_strdup(ptr->val));
+	}
+	ptr = vars;
+	while (ptr != NULL && _strcmp(ptr->name, name))
+	{
+#ifdef DEBUGMODE
+		printf("Checked .%s. against .%s.\n", name, ptr->name);
+#endif
+		ptr = ptr->next;
+	}
+	if (ptr == NULL)
+	{
+#ifdef DEBUGMODE
+		printf("Var not found %s\n", name);
+#endif
+		return (name);
+	}
+#ifdef DEBUGMODE
+	printf("Returning var %s\n", ptr->val);
+#endif
+	return (_strdup(ptr->val));
+}
+/**
+ * setsvar - sets shell var
+ * @name: name of var
+ * @val: value of var
+ * Return: int
+ */
+int setsvar(char *name, char *val)
+{
+	ShellVar **vars = getvars();
+	ShellVar *special = *(getspecial());
+	ShellVar *ptr = special, *new;
+
+#ifdef DEBUGMODE
+	printf("in setsvar, special address: %p\n", special);
+	printf("Got %s %s\n", name, val);
+#endif
+	while (_strcmp(ptr->name, name) && ptr->next != NULL)
+	{
+		ptr = ptr->next;
+	}
+	if (!_strcmp(ptr->name, name))
+	{
+#ifdef DEBUGMODE
+		printf("Setting special %s to %s\n", ptr->name, val);
+		printf("ptr -> val %p\n", ptr->val);
+#endif
+		free(ptr->val);
+		ptr->val = _strdup(val);
+		return (0);
+	}
+	ptr = *vars;
+#ifdef DEBUGMODE
+	printf("vars address %p\n", *vars);
+#endif
+	if (ptr == NULL)
+	{
+#ifdef DEBUGMODE
+		printf("Setting new list %s to %s\n", name, val);
+#endif
+		new = malloc(sizeof(ShellVar));
+		if (new == NULL)
+			return (-1);
+		new->name = _strdup(name);
+		new->val = _strdup(val);
+		new->next = NULL;
+		*vars = new;
+		return (0);
+	}
+	while (_strcmp(ptr->name, name) && ptr->next != NULL)
+		ptr = ptr->next;
+	if (ptr != NULL && !_strcmp(ptr->name, name))
+	{
+#ifdef DEBUGMODE
+		printf("Setting %s to %s\n", ptr->name, val);
+#endif
+		free(ptr->val);
+		ptr->val = _strdup(val);
+	}
+	else
+	{
+#ifdef DEBUGMODE
+		printf("Setting new %s to %s\n", name, val);
+#endif
+		new = malloc(sizeof(ShellVar));
+		if (new == NULL)
+			return (-1);
+		new->name = _strdup(name);
+		new->val = _strdup(val);
+		new->next = NULL;
+		ptr->next = new;
+	}
+	return (0);
+}
+/**
+ * unsetsvar - unset shell var
+ * @name: name of var to unset
+ * Return: int
+ */
+int unsetsvar(char *name)
+{
+	ShellVar *vars = *getvars();
+	ShellVar *ptr = vars, *next;
+
+#ifdef DEBUGMODE
+	printf("In unsetsvar:vars:%p:name:%s\n", vars, name);
+#endif
+	if (vars == NULL)
+		return (0);
+#ifdef DEBUGMODE
+	printf("ptr->name:%s\n", ptr->name);
+#endif
+	if (!_strcmp(ptr->name, name))
+	{
+#ifdef DEBUGMODE
+		printf("First node match\n");
+#endif
+		*vars = *vars->next;
+		free(ptr->name);
+		free(ptr->val);
+		free(ptr);
+		return (0);
+	}
+	while (ptr->next != NULL && _strcmp(ptr->next->name, name))
+		ptr = ptr->next;
+	if (!_strcmp(ptr->next->name, name))
+	{
+		next = ptr->next->next;
+		free(ptr->next->name);
+		free(ptr->next->val);
+		free(ptr->next);
+		ptr->next = next;
+	}
+	return (0);
+}
