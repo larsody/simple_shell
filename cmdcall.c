eaf5de89edd75054426a0fd28cecf155becbfbce
larsody <larsody@gmail.com>
2022-08-10 13:34:25 -0700

    update edited

--- cmdcall.c
+++ cmdcall.c
@@ -127,3 +127,88 @@ int cmdcall(char *av[], char *cmd)
 #endif
 	free(environ);
 	return (status);
+}
+/**
+ * builtincall - calls builtin commands
+ * @av: arguments
+ * Return: retval
+ */
+int builtincall(char *av[])
+{
+	int retval = 0;
+	char *retstr;
+
+	if (av[0] == NULL)
+		return (0);
+#ifdef DEBUGMODE
+	printf("In builtincall %p\n%s\n", av[0], av[0]);
+	printf("av[1]:%s\n", av[1]);
+#endif
+	if (!_strcmp(av[0], "exit"))
+	{
+		if (av[1] != NULL)
+			if (av[1][0] >= '0' && av[1][0] <= '9')
+			{
+				retval = _atoi(av[1]);
+				exitcleanup(av);
+				exit_hist();
+				exit(retval);
+			}
+			else
+			{
+				printerr(": exit: Illegal number: ");
+				fprintstrs(STDERR_FILENO, av[1], "\n", NULL);
+				retval = 2;
+			}
+		else
+		{
+			retstr = getsvar("?");
+			retval = _atoi(retstr);
+			free(retstr);
+			exitcleanup(av);
+			exit_hist();
+			exit(retval);
+		}
+	}
+	else if (!_strcmp(av[0], "cd"))
+		retval = _cd(av);
+/*
+ *
+ * else if (!_strcmp(av[0], "getenv"))
+ *	retval = !printf("%s\n", _getenv(av[1]));
+*/
+	else if (!_strcmp(av[0], "history"))
+		retval = print_hist();
+	else if (!_strcmp(av[0], "help"))
+		retval = help(av[1]);
+	else if (!_strcmp(av[0], "env"))
+		retval = _printenv();
+	else if (!_strcmp(av[0], "setenv"))
+		retval = _setenv(av[1], av[2]);
+	else if (!_strcmp(av[0], "unsetenv"))
+		retval = _unsetenv(av[1]);
+	else if (!_strcmp(av[0], "alias"))
+		retval = aliascmd(av);
+	else if (!_strcmp(av[0], "unset"))
+		retval = unsetsvar(av[1]);
+	else if (!_strcmp(av[0], "unalias"))
+		retval = unsetalias(av[1]);
+	else if (av[0][0] != '/' &&
+		 !(av[0][0] == '.' && (av[0][1] == '/' ||
+				       (av[0][1] == '.' && av[0][2] == '/'))))
+		retval = checkpath(av);
+	else
+		retval = cmdcall(av, av[0]);
+#ifdef DEBUGMODE
+	printf("After call back in builtin retval:%d\n", retval);
+#endif
+	if (retval % 256 == 0)
+		retval /= 256;
+	retstr = itos(retval % 128);
+#ifdef DEBUGMODE
+	printf("Status string:%s\n", retstr);
+#endif
+	setsvar("?", retstr);
+	free(retstr);
+	return (retval);
+}
